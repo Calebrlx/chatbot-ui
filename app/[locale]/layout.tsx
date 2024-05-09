c49652eb79cc3c1a45f36c3a9fa3a5eb42b6ddf6
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-05-08 18:53:19 -0800

    d

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "RelixAI"
+const APP_DEFAULT_TITLE = "RelixAI"
+const APP_TITLE_TEMPLATE = "%s - RelixAI"
+const APP_DESCRIPTION = "Chabot UI!"
 
 interface RootLayoutProps {
   children: ReactNode
