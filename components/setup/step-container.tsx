83e01cac3a03c6ff43d9daa711f0548f1208e1ec
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-05-21 15:12:57 -0800

    d

--- components/setup/step-container.tsx
+++ components/setup/step-container.tsx
@@ -42,7 +42,7 @@ export const StepContainer: FC<StepContainerProps> = ({
 
   return (
     <Card
-      className="max-h-[calc(100vh-60px)] w-[600px] overflow-auto"
+      className="max-h-[calc(100vh-60px)] w-full max-w-[600px] overflow-auto mx-auto p-4 md:p-6"
       onKeyDown={handleKeyDown}
     >
       <CardHeader>
@@ -87,3 +87,93 @@ export const StepContainer: FC<StepContainerProps> = ({
     </Card>
   )
 }
+
+// import { Button } from "@/components/ui/button"
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle
+// } from "@/components/ui/card"
+// import { FC, useRef } from "react"
+
+// export const SETUP_STEP_COUNT = 3
+
+// interface StepContainerProps {
+//   stepDescription: string
+//   stepNum: number
+//   stepTitle: string
+//   onShouldProceed: (shouldProceed: boolean) => void
+//   children?: React.ReactNode
+//   showBackButton?: boolean
+//   showNextButton?: boolean
+// }
+
+// export const StepContainer: FC<StepContainerProps> = ({
+//   stepDescription,
+//   stepNum,
+//   stepTitle,
+//   onShouldProceed,
+//   children,
+//   showBackButton = false,
+//   showNextButton = true
+// }) => {
+//   const buttonRef = useRef<HTMLButtonElement>(null)
+
+//   const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+//     if (e.key === "Enter" && !e.shiftKey) {
+//       if (buttonRef.current) {
+//         buttonRef.current.click()
+//       }
+//     }
+//   }
+
+//   return (
+//     <Card
+//       className="max-h-[calc(100vh-60px)] w-[600px] overflow-auto"
+//       onKeyDown={handleKeyDown}
+//     >
+//       <CardHeader>
+//         <CardTitle className="flex justify-between">
+//           <div>{stepTitle}</div>
+
+//           <div className="text-sm">
+//             {stepNum} / {SETUP_STEP_COUNT}
+//           </div>
+//         </CardTitle>
+
+//         <CardDescription>{stepDescription}</CardDescription>
+//       </CardHeader>
+
+//       <CardContent className="space-y-4">{children}</CardContent>
+
+//       <CardFooter className="flex justify-between">
+//         <div>
+//           {showBackButton && (
+//             <Button
+//               size="sm"
+//               variant="outline"
+//               onClick={() => onShouldProceed(false)}
+//             >
+//               Back
+//             </Button>
+//           )}
+//         </div>
+
+//         <div>
+//           {showNextButton && (
+//             <Button
+//               ref={buttonRef}
+//               size="sm"
+//               onClick={() => onShouldProceed(true)}
+//             >
+//               Next
+//             </Button>
+//           )}
+//         </div>
+//       </CardFooter>
+//     </Card>
+//   )
+// }
